58eb501375021a68e4c0260f7a9b9e0d4ca1e63b
Avrel OWONA <guy.owona@bhent.net>
2022-11-02 01:52:55 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import {FaFacebook, FaTwitter, FaYoutube} from "react-icons/fa";
+import {FaFacebook, FaInstagram, FaTwitter, FaYoutube} from "react-icons/fa";
 import {RiWhatsappFill} from 'react-icons/ri'
 
 export default function Home() {
@@ -14,10 +14,10 @@ export default function Home() {
       <h1 className="font-bold text-3xl sm:text-5xl lg:text-8xl uppercase text-gray-100">Coming Soon</h1>
       {/* <h3 className="uppercase mt-10 text-gray-200">under construction new website</h3> */}
       <div className="text-white mt-10 lg:text-3xl flex items-center focus-visible:outline-none">
+              <a href={'#'}><FaYoutube className={'ml-5 hover:text-gray-600 cursor-pointer'}/></a>
+              <a href={'#'}><FaInstagram className={'ml-5 hover:text-gray-600 cursor-pointer'}/></a>
               <a href={'#'}><FaFacebook className={'hover:text-gray-600 cursor-pointer'}/></a>
               <a href={'#'}><FaTwitter className={'ml-7 hover:text-gray-600 cursor-pointer'}/></a>
-              <a href={'#'}><FaYoutube className={'ml-5 hover:text-gray-600 cursor-pointer'}/></a>
-              <a href={'#'}><RiWhatsappFill className={'ml-5 hover:text-gray-600 cursor-pointer'}/></a>
           </div>
       </div>
     </div>
