f09d78d201c78802d3fb186c0b3d340b727090ee
Avrel OWONA <guy.owona@bhent.net>
2022-11-02 02:53:36 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import {FaFacebook, FaInstagram, FaTwitter, FaYoutube} from "react-icons/fa";
+import {FaFacebook, FaInstagram, FaTiktok, FaTwitter, FaYoutube} from "react-icons/fa";
 import {RiWhatsappFill} from 'react-icons/ri'
 
 export default function Home() {
@@ -14,10 +14,11 @@ export default function Home() {
       <h1 className="font-bold text-3xl sm:text-5xl lg:text-8xl uppercase text-gray-100">Coming Soon</h1>
       {/* <h3 className="uppercase mt-10 text-gray-200">under construction new website</h3> */}
       <div className="text-white mt-10 lg:text-3xl flex items-center focus-visible:outline-none">
-              <a href={'#'}><FaYoutube className={'ml-5 hover:text-gray-600 cursor-pointer'}/></a>
-              <a href={'#'}><FaInstagram className={'ml-5 hover:text-gray-600 cursor-pointer'}/></a>
-              <a href={'#'}><FaFacebook className={'hover:text-gray-600 ml-5 cursor-pointer'}/></a>
-              <a href={'#'}><FaTwitter className={'ml-7 hover:text-gray-600 cursor-pointer'}/></a>
+              <a target="_blank" href={'https://www.youtube.com/c/tchinda'}><FaYoutube className={'ml-5 hover:text-gray-600 cursor-pointer'}/></a>
+              <a target="_blank" href={'https://www.instagram.com/tchindax'}><FaInstagram className={'ml-5 hover:text-gray-600 cursor-pointer'}/></a>
+              <a target="_blank" href={'https://www.facebook.com/tchindax'}><FaFacebook className={'hover:text-gray-600 ml-5 cursor-pointer'}/></a>
+              <a target="_blank" href={'https://www.tiktok.com/@tchinda_'}><FaTiktok className={'hover:text-gray-600 ml-5 cursor-pointer'}/></a>
+              <a target="_blank" href={'https://www.twitter.com/tchinda_'}><FaTwitter className={'ml-7 hover:text-gray-600 cursor-pointer'}/></a>
           </div>
       </div>
     </div>
