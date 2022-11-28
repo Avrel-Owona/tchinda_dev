219f088dbd256a21bb5cb87c58bfbd611590f1bd
Avrel OWONA <guy.owona@bhent.net>
2022-11-28 02:12:13 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,15 @@
-import { useRouter } from "next/router";
+import { useRouter } from "next/router"
 
 const Footer = () => {
-    const Router = useRouter()
-    return(
-        <footer className={`py-10 ${Router.asPath !== '/' ? null : 'mt-20 border-gray-500 border-t'} text-center text-gray-300 `}>
-                <span className="font-thin text-sm">© {new Date().getFullYear()} Avrel OWONA. Tous les droits reservés</span>
-            </footer>
-    )
+	const Router = useRouter()
+	return (
+		<footer
+			className={`py-10 mt-20 border-gray-600 border-t text-center text-gray-300 `}
+		>
+			<span className="font-thin text-xs">
+				© {new Date().getFullYear()} Avrel OWONA. Tous les droits reservés
+			</span>
+		</footer>
+	)
 }
-export default Footer;
+export default Footer
