cb6b4c649875f8281121947b21c22c4ecb0693b8
Avrel OWONA <guy.owona@bhent.net>
2022-11-28 02:49:49 +0100

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -12,7 +12,49 @@ const Layout = ({ children }) => {
 					href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,800&family=Oswald:wght@200;300;400;600;700&display=swap"
 					rel="stylesheet"
 				/>
-				<title>PatricK TCHINDA</title>
+				<title>TCHINDA</title>
+				<meta
+					name="description"
+					content="Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
+							Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum.
+							Aenean imperdiet."
+				/>
+				<meta property="og:title" content="TCHINDA" key="ogtitle" />
+				<meta
+					property="og:description"
+					content="Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
+							Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum.
+							Aenean imperdiet."
+					key="ogdesc"
+				/>
+				<meta property="og:image" content="/IMG_9345.JPG" />
+
+				{/* Twitter */}
+				<meta name="twitter:site" content="tournonslapagecameroun.org" />
+				<meta name="twitter:title" content="TCHINDA" />
+				<meta name="twitter:image:src" content="/IMG_9345.JPG" />
+				<meta name="twitter:card" content="summary_large_image" key="twcard" />
+				<meta name="twitter:creator" key="twhandle" />
+				<meta
+					name="twitter:description"
+					content="Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
+							Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum.
+							Aenean imperdiet."
+				/>
+
+				{/* Open Graph */}
+				<meta property="og:type" content="openGraphType" key="ogurl" />
+				<meta property="og:url" content="tchinda.com" key="ogurl" />
+				<meta property="og:image" content="/IMG_9345.JPG" key="ogimage" />
+				<meta property="og:site_name" content="tchinda.org" key="ogsitename" />
+				<meta property="og:title" content="TCHINDA" key="ogtitle" />
+				<meta
+					property="og:description"
+					content="Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
+							Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum.
+							Aenean imperdiet."
+					key="ogdesc"
+				/>
 			</Head>
 			<Navbar />
 			{children}
